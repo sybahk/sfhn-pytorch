fixed single node ddp, compile and mixed precision training

ptsr/model/_utils.py
@@ -66,18 +66,22 @@ def conv3x3(
     )
 
 
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+def conv1x1(
+    in_planes: int, out_planes: int, stride: int = 1, bias: bool = False
+) -> nn.Conv2d:
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=bias)
 
 
 class FreqL1Loss(nn.Module):
     def forward(self, x, target):
+        x = x.float()
+        target = target.float()
         pix_l1 = F.l1_loss(x, target)
-        x = torch.fft.fft2(x, norm="ortho")
-        x = torch.cat(x.real, x.imag, dim=1)
-        target = torch.fft.fft2(target, norm="ortho")
-        target = torch.cat(target.real, target.imag, dim=1)
+        x = torch.fft.rfft2(x, norm="ortho")
+        x = torch.cat((x.real, x.imag), dim=1)
+        target = torch.fft.rfft2(target, norm="ortho")
+        target = torch.cat((target.real, target.imag), dim=1)
         return pix_l1 + F.l1_loss(x, target) * 0.1
 
 

ptsr/model/common.py
@@ -312,9 +312,9 @@ class SFHNBlock(ResidualBase):
             SEBlock(planes, reduction, act_mode),
         )
         self.freq = nn.Sequential(
-            conv1x1(planes * 2, planes * 2, bias=bias),
+            conv1x1(planes * 2, planes * 2, bias=False),
             get_activation(act_mode),
-            conv1x1(planes * 2, planes * 2, bias=bias),
+            conv1x1(planes * 2, planes * 2, bias=False),
         )
 
         # normal initialization
@@ -323,13 +323,13 @@ class SFHNBlock(ResidualBase):
                 nn.init.normal_(self.body[idx].weight, 0.0, normal_init_std)
 
     def _forward_res(self, x: torch.Tensor) -> torch.Tensor:
-        freq_x = x
-        freq_x = torch.fft.fft2(freq_x, norm="ortho")
-        freq_x = torch.cat(freq_x.real, freq_x.imag, dim=1)
-        freq_x = self.freq(freq_x)
+        freq_x = torch.fft.rfft2(x.float(), norm="ortho")
+        freq_x = torch.cat((freq_x.real, freq_x.imag), dim=1)
+        freq_x = self.freq(freq_x).float()
         freq_x_real, freq_x_imag = freq_x.chunk(2, 1)
         freq_x = torch.complex(freq_x_real, freq_x_imag)
-        freq_x = torch.fft.ifft2(freq_x, norm="ortho")
+        freq_x = torch.fft.irfft2(freq_x, norm="ortho")
+
         x = (self.body(x) + freq_x).mul(self.res_scale)
         return x
 
@@ -435,8 +435,7 @@ class MeanShift(nn.Conv2d):
         std = torch.Tensor(rgb_std)
         self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
         self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
-        for p in self.parameters():
-            p.requires_grad = False
+        self.requires_grad_(False)
 
 
 class Affine2d(nn.Module):

ptsr/utils/solver/build.py
@@ -94,6 +94,7 @@ def maybe_add_gradient_clipping(
     optimizer.__class__ = OptimizerWithGradientClip
     return optimizer
 
+
 # 1. Build Optimizer
 
 
@@ -134,53 +135,54 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
                 lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.BIAS_LR_FACTOR
                 weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
             # Scale learning rate for residual blocks
-            if res_factor is not None: 
-                prefix = module_name.split('.')[:6]
-                prefix = '.'.join(prefix)
+            if res_factor is not None:
+                prefix = module_name.split(".")[:6]
+                prefix = ".".join(prefix)
                 if prefix in res_name_list:
                     lr *= res_factor
 
-            params += [{"params": [value], "lr": lr,
-                        "weight_decay": weight_decay}]
+            params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
     solver_name = cfg.SOLVER.NAME
-    if solver_name == 'SGD':
+    if solver_name == "SGD":
         optimizer = torch.optim.SGD(
-            params, cfg.SOLVER.BASE_LR, momentum=cfg.SOLVER.MOMENTUM)
-    elif solver_name in ['Adam', 'AdamW']:
+            params, cfg.SOLVER.BASE_LR, momentum=cfg.SOLVER.MOMENTUM
+        )
+    elif solver_name in ["Adam", "AdamW"]:
         optimizer = getattr(torch.optim, solver_name)(
-            params, cfg.SOLVER.BASE_LR, betas=cfg.SOLVER.BETAS)
-    elif solver_name == 'LARS': # Large-batch version of SGD
+            params, cfg.SOLVER.BASE_LR, betas=cfg.SOLVER.BETAS
+        )
+    elif solver_name == "LARS":  # Large-batch version of SGD
         optimizer = LARS(params, cfg.SOLVER.BASE_LR, momentum=cfg.SOLVER.MOMENTUM)
-    elif solver_name == 'Lamb': # Large-batch version of Adam
-        if cfg.MODEL.MIXED_PRECESION:
-            optimizer = Lamb16(
-                params, cfg.SOLVER.BASE_LR, betas=cfg.SOLVER.BETAS,
-                eps=cfg.SOLVER.EPS, clamp_trust_ratio=cfg.SOLVER.CLAMP_TRUST_RATIO,
-            )
-        else:
-            optimizer = Lamb(params, cfg.SOLVER.BASE_LR, betas=cfg.SOLVER.BETAS)
+    elif solver_name == "Lamb":  # Large-batch version of Adam
+        # if cfg.MODEL.MIXED_PRECESION:
+        #     optimizer = Lamb16(
+        #         params, cfg.SOLVER.BASE_LR, betas=cfg.SOLVER.BETAS,
+        #         eps=cfg.SOLVER.EPS, clamp_trust_ratio=cfg.SOLVER.CLAMP_TRUST_RATIO,
+        #     )
+        # else:
+        optimizer = Lamb(params, cfg.SOLVER.BASE_LR, betas=cfg.SOLVER.BETAS)
     else:
-        raise ValueError(
-            "Solver type {} is not supported!".format(solver_name))
+        raise ValueError("Solver type {} is not supported!".format(solver_name))
 
     optimizer = maybe_add_gradient_clipping(cfg, optimizer)
-    print('Optimizer: ', optimizer.__class__.__name__)
+    print("Optimizer: ", optimizer.__class__.__name__)
     return optimizer
+
 
 def _create_residual_name(cfg):
     if cfg.SOLVER.RESIDUAL_LR_FACTOR is None:
         return None, None
 
-    assert cfg.MODEL.BLOCK_TYPE in [
-        'rcan_block', 'rcan_block_dw', 'rcan_block_all_dw']
+    assert cfg.MODEL.BLOCK_TYPE in ["rcan_block", "rcan_block_dw", "rcan_block_all_dw"]
 
     name_list = []
-    prefix = 'module.model.body.'
+    prefix = "module.model.body."
     for i in range(cfg.MODEL.N_RESGROUPS):
         for j in range(cfg.MODEL.N_RESBLOCKS):
-            name_list.append(prefix + str(i) + '.body.' + str(j))
+            name_list.append(prefix + str(i) + ".body." + str(j))
     return name_list, cfg.SOLVER.RESIDUAL_LR_FACTOR
+
 
 # 2. Build LR Scheduler
 
@@ -212,28 +214,34 @@ def build_lr_scheduler(
         )
     elif name == "MultiStepLR":
         return MultiStepLR(
-            optimizer,
-            milestones=cfg.SOLVER.STEPS,
-            gamma=cfg.SOLVER.GAMMA
+            optimizer, milestones=cfg.SOLVER.STEPS, gamma=cfg.SOLVER.GAMMA
         )
     elif name == "ReduceLROnPlateau":
         return ReduceLROnPlateau(
             optimizer,
-            mode='min', factor=cfg.SOLVER.GAMMA, patience=1000,
-            threshold=0.001, threshold_mode='rel', cooldown=0,
-            min_lr=1e-06, eps=1e-08
+            mode="min",
+            factor=cfg.SOLVER.GAMMA,
+            patience=1000,
+            threshold=0.001,
+            threshold_mode="rel",
+            cooldown=0,
+            min_lr=1e-06,
+            eps=1e-08,
         )
     else:
         raise ValueError("Unknown LR scheduler: {}".format(name))
 
+
 # 3. Build SWA model
 
 
-def build_swa_model(cfg: CfgNode,
-                    model: torch.nn.Module,
-                    optimizer: torch.optim.Optimizer,
-                    device: torch.device,
-                    is_pretrained: bool = False):
+def build_swa_model(
+    cfg: CfgNode,
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+    is_pretrained: bool = False,
+):
     # Instead of copying weights during initialization, the SWA model copys
     # the model weights when self.update_parameters is first called.
     # https://github.com/pytorch/pytorch/blob/1.7/torch/optim/swa_utils.py#L107
@@ -243,7 +251,7 @@ def build_swa_model(cfg: CfgNode,
     swa_model = AveragedModel(model).to(device)
     if is_pretrained:
         swa_model.update_parameters(model)
-        
+
     lr = cfg.SOLVER.BASE_LR
     lr *= cfg.SOLVER.SWA.LR_FACTOR
     swa_scheduler = SWALR(optimizer, swa_lr=lr)

ptsr/utils/trainer.py
@@ -77,7 +77,7 @@ class Trainer:
 
             # full-precision when finetuning tail
             autocast_enabled = self.mixed_fp and self.tail_only_iter <= 0
-            with autocast(enabled=autocast_enabled):
+            with autocast(enabled=autocast_enabled, dtype=torch.bfloat16):
                 sr = self.model(lr)
                 loss = self.loss(sr, hr)
 
@@ -93,6 +93,9 @@ class Trainer:
             self.scheduler_step(i)  # update SWA model if it exists
             if (self.rank is None or self.rank == 0) and i % 10 == 0:
                 self.log_train(i, loss, timer)
+
+                if i + 1 == 1:
+                    timer.tic()  # remove compile time
 
             del lr, hr, sr, loss  # Release some GPU memory
 